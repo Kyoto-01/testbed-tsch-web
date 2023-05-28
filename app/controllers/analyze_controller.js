da59984d49d4fb573859b25506110bc4efe32797
Kyoto-01 <cameraman269@gmail.com>
2023-05-28 19:41:27 -0300

    add status, start at and stop at infos in experiments/ and experiment/ screens

--- app/controllers/analyze_controller.js
+++ app/controllers/analyze_controller.js
@@ -3,12 +3,36 @@ import analyzeApi from "../apis/analyze_api.js";
 
 async function get_experiments(req, res) {
     try {
-        const response = await analyzeApi.get_experiments();
 
-        if (response.status == 200) {
-            const context = {
-                "experiments": response.body
+        const experimentsName = await analyzeApi.get_experiments();
+
+        const experimentsData = [];
+
+        if (experimentsName.status == 200) {
+
+            for (let exp of experimentsName.body) {
+
+                let expData = await analyzeApi.get_report_summary(exp);
+    
+                if (expData.status == 200) {
+                    
+                    expData = {
+                        "name": expData.body.testbed.name,
+                        "status": expData.body.testbed.status == "start" ? "Running" : "Stopped",
+                        "start": new Date(expData.body.testbed.start * 1000),
+                        "stop": expData.body.testbed.stop ? new Date(expData.body.testbed.stop * 1000) : "-"
+                    };
+    
+                    experimentsData.push(expData);
+                }
             }
+
+            // from most current to oldest experiment sort
+            experimentsData.sort((a, b) => (a.start < b.start) ? 1 : -1);
+
+            const context = {
+                "experiments": experimentsData
+            };
 
             res.render("pages/experiments", context);
 
@@ -17,6 +41,7 @@ async function get_experiments(req, res) {
         }
 
     } catch {
+
         res.render(
             "pages/message",
             { "msg": "Failed to get experiments." }
@@ -33,12 +58,15 @@ async function get_report_summary(req, res) {
         if (response.status == 200) {
             const context = {
                 "experiment_name": response.body.testbed.name,
+                "experiment_status": response.body.testbed.status == "start" ? "Running" : "Stopped",
+                "experiment_start": new Date(response.body.testbed.start * 1000),
+                "experiment_stop": response.body.testbed.stop ? new Date(response.body.testbed.stop * 1000) : "-",
                 "client_motes": response.body.testbed.client,
                 "server_motes": response.body.testbed.server
             };
 
             res.render("pages/experiment", context);
-            
+
         } else {
             throw new Error;
         }
